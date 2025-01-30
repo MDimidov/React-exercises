fa8e2a6d681fed12949152f21a8992b0d270ff3b
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-01-30 16:41:49 +0200

    Create first-react-app

---
+++ 1. Intro to React and JSX/first-react-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>LiveTime reset</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <h1>Hello React</h1>
+        <h2>Let's start learning</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
