935dc5ab9e669687834c72a135c22bde8a0bebe9
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-02-01 10:38:57 +0200

    Update array using map to show movie

--- 3. Components Deep Dive/components-deep-dive-demo/src/components/MovieList.js
+++ 3. Components Deep Dive/components-deep-dive-demo/src/components/MovieList.js
@@ -2,16 +2,31 @@ import Movie from "./Movie";
 import React from "react";
 
 function MovieList({ movies }) {
-  let movieList = [];
-  for (const movie of movies) {
-    // movieList.push(React.createElement(Movie, movie));
-    movieList.push(
-      <li>
-        <Movie {...movie} />
-      </li>
-    );
-  }
-  return <ol>{movieList}</ol>;
+  // let movieList = [];
+  // for (const movie of movies) {
+  //   // movieList.push(React.createElement(Movie, movie));
+  //   movieList.push(
+  //     <li>
+  //       <Movie {...movie} />
+  //     </li>
+  //   );
+  // }
+
+  // let movieList = movies.map((movie) => (
+  //   <li>
+  //     <Movie {...movie} />
+  //   </li>
+  // ));
+
+  return (
+    <ol>
+      {movies.map((movie) => (
+        <li>
+          <Movie {...movie} />
+        </li>
+      ))}
+    </ol>
+  );
 }
 
 export default MovieList;
