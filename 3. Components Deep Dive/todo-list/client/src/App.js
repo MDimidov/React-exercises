f2f7c8d63d2b8e29b0ec2605a109ffbce5937a44
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-02-02 02:48:26 +0200

    Add logic for loading bar Add new task

--- 3. Components Deep Dive/todo-list/client/src/App.js
+++ 3. Components Deep Dive/todo-list/client/src/App.js
@@ -1,17 +1,19 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-// import Loading from "./components/Loading";
+import Loading from "./components/Loading";
 import TodoList from "./components/TodoList";
 import { useEffect, useState } from "react";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch(`http://localhost:3030/jsonstore/todos`)
       .then((res) => res.json())
       .then((data) => {
         setTodos(Object.values(data));
+        setIsLoading(false)
       });
   }, []);
 
@@ -22,6 +24,13 @@ function App() {
       )
     );
   };
+
+  function addTodoHandler() {
+    const lastId = Number(todos[todos.length - 1]._id.slice(5));
+    const text = prompt('Add new Task:');
+    const task = {_id: `todo_${lastId + 1}`, text, isCompleted: false};
+    setTodos(state => [task, ...state]);
+  }
 
   return (
     <div className="App">
@@ -35,14 +44,14 @@ function App() {
           <h1>Todo List</h1>
 
           <div className="add-btn-container">
-            <button className="btn">+ Add new Todo</button>
+            <button className="btn" onClick={() => addTodoHandler({})}>+ Add new Todo</button>
           </div>
 
           <div className="table-wrapper">
-            {/* <Loading /> */}
-
-            {/* <!-- Todo list table --> */}
+            {isLoading ? 
+            <Loading /> :
             <TodoList todos={todos} toggleStatus={toggleStatus} />
+            }
           </div>
         </section>
       </main>
