a32b13b3802ee1af459448e8fb75851eca9140d9
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-02-06 14:23:12 +0200

    Practice NavLink & useNavigate

--- 6. Routing/react-router-demo/src/components/Character.js
+++ 6. Routing/react-router-demo/src/components/Character.js
@@ -1,22 +1,28 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const baseUrl = 'https://swapi.dev/api/people/';
 
 export function Character({ name, url }) {
 
     const { characterId } = useParams();
+    const navigate = useNavigate()
     const [character, setCharacter] = useState({});
 
     useEffect(() => {
         fetch(baseUrl + characterId)
         .then(res => res.json())
-        .then(setCharacter)
-    })
+        .then(setCharacter);
+    }, [characterId]);
+
+    function onBackBtnClick() {
+        navigate('/characters');
+    }
     return (
         <>
             <h1>Id: {characterId}</h1>
             <h1>Name: {character.name}</h1>
+            <button onClick={onBackBtnClick}>Back to character list</button>
         </>
     );
 }
--- 6. Routing/react-router-demo/src/components/Navigation.js
+++ 6. Routing/react-router-demo/src/components/Navigation.js
@@ -1,13 +1,13 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './Navigation.module.css';
 
 export function Navigation() {
     return (
         <nav className={styles['navigation']}>
             <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/characters'>Characters</Link></li>
+                <li><NavLink style={({isActive}) => ({color: isActive ? 'purple' : ''})} to="/">Home</NavLink></li>
+                <li><NavLink style={({isActive}) => ({color: isActive ? 'purple' : ''})} to='/about'>About</NavLink></li>
+                <li><NavLink style={({isActive}) => ({color: isActive ? 'purple' : ''})} to='/characters'>Characters</NavLink></li>
             </ul>
         </nav>
     )
