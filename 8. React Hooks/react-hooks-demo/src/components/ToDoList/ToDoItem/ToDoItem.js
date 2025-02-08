fc96aced7a71d02aff97ad9734cbb61cb8b4c532
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-02-08 17:53:44 +0200

    Add delete fetch request

--- 8. React Hooks/react-hooks-demo/src/components/ToDoList/ToDoItem/ToDoItem.js
+++ 8. React Hooks/react-hooks-demo/src/components/ToDoList/ToDoItem/ToDoItem.js
@@ -1,8 +1,18 @@
 import ListGroup from "react-bootstrap/esm/ListGroup";
 import Button from 'react-bootstrap/Button';
 
-export default function TodoItem({ text }) {
+const baseUrl = 'http://localhost:3030/jsonstore/todo-items/';
 
+export default function TodoItem({ _id, text }) {
+
+    async function onDeleteTask(_id) {
+        await fetch(baseUrl + _id, {
+            method: 'DELETE',
+            header: {
+                'Content-Type': 'application/json',
+            }
+        });
+    }
     return (
         <ListGroup.Item style={{
             display: 'flex',
@@ -10,7 +20,7 @@ export default function TodoItem({ text }) {
         }}>
 
             {text}
-            <Button variant="danger">x</Button>
+            <Button variant="danger" onClick={() => onDeleteTask(_id)}>x</Button>
         </ListGroup.Item>
     );
 };
