0192963b11393f8772e16d87012c3d8b9db7869a
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-02-13 16:56:41 +0200

    Test

--- 10. Workshop Advanced Techniques/class-components/src/App.js
+++ 10. Workshop Advanced Techniques/class-components/src/App.js
@@ -1,6 +1,11 @@
+import ListElement from "./components/ListElement";
+import { Navigation } from "./components/Navigation";
+
 function App() {
   return (
     <div className="App">
+      <Navigation />
+      <ListElement />
     </div>
   );
 }
---
+++ 10. Workshop Advanced Techniques/class-components/src/components/ListElement.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+import ListGroup from 'react-bootstrap/ListGroup';
+
+export default class ListElement extends Component {
+    render() {
+        return (
+            <ListGroup>
+                <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+                <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+            </ListGroup>
+        );
+    }
+}
