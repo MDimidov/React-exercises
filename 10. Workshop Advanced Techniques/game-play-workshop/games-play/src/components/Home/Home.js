779411c6ebe1e3834061c9ad28926b8c93694ed9
Mariyan <116071995+MDimidov@users.noreply.github.com>
2025-02-16 16:58:33 +0200

    Update latestGames

--- 10. Workshop Advanced Techniques/game-play-workshop/games-play/src/components/Home/Home.js
+++ 10. Workshop Advanced Techniques/game-play-workshop/games-play/src/components/Home/Home.js
@@ -3,7 +3,10 @@ import LatestGames from "./LatestGames";
 
 export function Home() {
 
-    const { games } = useGameContext()
+    const { games } = useGameContext();
+
+    let latestGames = [...games];
+    latestGames = latestGames.sort((a, b) => (b._createdOn - a._createdOn)).splice(0, 3);
 
     return (
         <section id="welcome-world">
@@ -18,12 +21,12 @@ export function Home() {
                 <h1>Latest Games</h1>
 
                 {/* <!-- Display div: with information about every game (if any) --> */}
-                {games.map(g =>
+                {latestGames.map(g =>
                     <LatestGames key={g._id} {...g} />
                 )}
 
                 {/* <!-- Display paragraph: If there is no games  --> */}
-                {games.Length && <p className="no-articles">No games yet</p>}
+                {latestGames.length === 0 && <p className="no-articles">No games yet</p>}
             </div>
         </section>
     );
--- 10. Workshop Advanced Techniques/game-play-workshop/games-play/src/services/gameServices.js
+++ 10. Workshop Advanced Techniques/game-play-workshop/games-play/src/services/gameServices.js
@@ -1,35 +1,39 @@
 import { requestFactory } from "./request";
 
-const baseUrl = 'http://localhost:3030/data/games/';
+const host = process.env.NODE_ENV === 'development' ? 
+    'http://localhost:3030' :
+    'http://localhost:3030'
+
+const url = host + '/data/games/';
 
 export function gameServiceFactory(token) {
 
     const request = requestFactory(token);
 
     async function getAllGames() {
-        const result = await request.get(baseUrl);
+        const result = await request.get(url);
         const games = Object.values(result);
 
         return games;
     }
 
     async function createGame(game) {
-        const result = await request.post(baseUrl, game);
+        const result = await request.post(url, game);
         return result;
     }
 
     async function editGame(gameId, game) {
-        const result = await request.put(baseUrl + gameId, game);
+        const result = await request.put(url + gameId, game);
         return result;
     }
 
     async function getGameById(gameId) {
-        const result = await request.get(baseUrl + gameId);
+        const result = await request.get(url + gameId);
         return result;
     }
 
     async function deleteGame(gameId) {
-        const result = await request.delete(baseUrl + gameId);
+        const result = await request.delete(url + gameId);
         return result;
     }
 
